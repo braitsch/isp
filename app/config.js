3fdf5d2db7d062cce1100a60df9ef3fc817f30bf
Stephen Braitsch <stephen@quietless.com>
2012-06-17 11:52:07 -0700

    adding jquery mobile and server side ua detection

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ module.exports = function(app, exp) {
 	app.configure(function(){
 		app.set('views', app.root + '/app/server/views');
 		app.set('view engine', 'jade');
-		app.set('view options', { pretty: true });
+		app.set('view options', { pretty: true, layout : false });
 		app.use(exp.bodyParser());
 		app.use(exp.methodOverride());
 		app.use(require('stylus').middleware({ src: app.root + '/app/public' }));
--- app/server/router.js
+++ app/server/router.js
@@ -2,7 +2,11 @@
 module.exports = function(app) {
 
 	app.get('/', function(req, res) {
-		res.render('index', { title: 'Comcast System Status', ip:req.connection.remoteAddress});
+		if (/mobile/i.test(req.headers['user-agent'])){
+			res.render('mobile', { title: 'Comcast System Status', layout: 'layout.m.jade', ip:req.connection.remoteAddress});
+		}	else{
+			res.render('desktop', { title: 'Comcast System Status', layout: 'layout.d.jade', ip:req.connection.remoteAddress});
+		}
 	});
 	
 	app.get('*', function(req, res) {
