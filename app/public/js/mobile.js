c58389193957f2e093a94707a97f9532f0453150
Stephen Braitsch <stephen@quietless.com>
2012-07-03 10:19:33 -0700

    home/info modal windows

--- app/public/js/mobile.js
+++ app/public/js/mobile.js
@@ -4,8 +4,10 @@ $(document).ready(function(){
 // user specific variables //
 	var ip, isp, loc, status;
 
-	var w1 =$('#welcome1');
-	var w2 =$('#welcome2');
+	var w1 = $('#modal-wel1');
+	var w2 = $('#modal-wel2');
+	var m1 = $('#modal-home');
+	var m2 = $('#modal-info');
 	
 	var isps = ['Comcast', 'AT&T', 'Verizon', 'Other'];
 		
@@ -13,16 +15,6 @@ $(document).ready(function(){
 	{
 		isp = $(e.target).text();
 		$('#isp-dropdown-label').text(isp);
-	}
-	
-	function onHomeSelection()
-	{
-		console.log('home');
-	}
-	
-	function onInfoSelection()
-	{
-		console.log('info');
 	}
 	
 	function detectLocation()
@@ -42,15 +34,22 @@ $(document).ready(function(){
 					"It looks like you're in beautiful<br><span style='color:#27A3FA'>"+c +', '+ s+"</span><br>How's your Internet connection today?"
 				)
 				w1.modal('show');
+				w1.find('button').click(onW1Click);
 			}
 		});
 	}
 	
 	function onW1Click(e)
 	{
-		status = $(e.target).attr('id') === 'service-ok' ? 1 : 0;
 		w1.modal('hide');
-	// build isp list based on location //
+		status = $(e.target).attr('id') === 'service-ok' ? 1 : 0;
+	// update status message //
+		if (status == 1){
+			w2.find('.modal-body p').html("Cool, glad to hear all is A-OK.<br>Who is your Internet Service Provider?");
+		}	else{
+			w2.find('.modal-body p').html("That sucks you're having problems.<br>Who is your Internet Service Provider?");
+		}
+	// build list of isp's based on location //
 		$('#isp-selector').empty();
 		$('#isp-dropdown ul').empty();
 		for (var i=0; i < isps.length; i++)  {
@@ -58,11 +57,13 @@ $(document).ready(function(){
 			$('#isp-dropdown ul').append("<li><a href='#'>"+isps[i]+"</a></li>");
 		};
 		$('#isp-selector button').click(function(e){
+			w2.find('#check-status').removeClass('disabled');
+			w2.find('#check-status').click(function(){ w2.modal('hide'); $('#header').show() });
 			var target = e.target; onIspSelection(e);
 			$('#isp-selector button').each(function(n, o){
 				if (o != target){
 					$(o).attr('class', 'btn');
-				}	else{
+				} else{
 					$(o).attr('class', 'btn btn-success');
 				}
 			})
@@ -72,15 +73,14 @@ $(document).ready(function(){
 
 // global nav //
 	$('#isp-dropdown ul').click(onIspSelection);
-	$('#btn-home').click(onHomeSelection);
-	$('#btn-info').click(onInfoSelection);
+	$('#btn-home').click(function(){ m1.modal('show'); });
+	$('#btn-info').click(function(){ m2.modal('show'); });
 	
 // modal window //
 	w1.modal({ show : false, keyboard : false, backdrop : 'static' });
-	w1.find('button').click(onW1Click);
 	w2.modal({ show : false, keyboard : false, backdrop : 'static' });
-	w2.find('#check-status').click(function(){ w2.modal('hide'); });
-
+	m1.modal({ show : false, keyboard : true, backdrop : true });
+	m2.modal({ show : false, keyboard : true, backdrop : true });
 	detectLocation();
 
 });
