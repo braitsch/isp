56b49317d08095fe254d5f4eaa922defe719d737
Stephen Braitsch <stephen@quietless.com>
2012-07-04 16:17:37 -0700

    info window bug fix

--- app/public/js/GoogleMap.js
+++ app/public/js/GoogleMap.js
@@ -141,7 +141,7 @@ GoogleMap = function()
 			$('#map_window #isp').html(m.isp + ' : '+status);
 			$('#map_window #time').html('Last Updated : ' + moment(parseInt(m.time)).fromNow());
 		//	win.setOptions({ boxClass : (m.special ? 'map_window_gradient': 'map_window_solid') });
-			win.open(map, m); win.show();
+			$('#map_window').show(); win.open(map, m); win.show();
 		});
 	}
 
