c8357f065d95c34f3dcb61383d69102e353e6fdd
Stephen Braitsch <stephen@quietless.com>
2012-07-04 13:10:03 -0700

    search area color updating based on average status

--- app/public/js/GoogleMap.js
+++ app/public/js/GoogleMap.js
@@ -2,6 +2,7 @@
 GoogleMap = function()
 {
 	var markers = [];
+	var searchCircle;
 	var searchArea = 1; // 1 mile
 	var map = new google.maps.Map(document.getElementById('map_canvas'), {
 		zoom : 14,
@@ -51,8 +52,24 @@ GoogleMap = function()
 			type : "POST",
 			data : {ne : ne, sw : sw},
 			success: function(data){
-	// attach markers that exist within the maps new boundaries //
-				for (var i = data.length - 1; i >= 0; i--) addMarker(data[i]);
+				var markersInCircle = [];
+				for (var i = data.length - 1; i >= 0; i--) {
+		// attach markers that exist within the maps new boundaries //
+					addMarker(data[i]);
+		// detect which markers are within the search area so we can draw an average status value //
+					if (searchCircle.contains(new google.maps.LatLng(data[i].lat, data[i].lng))) markersInCircle.push(data[i]);
+				}
+				var avgStatus = 0;
+				for (var i = markersInCircle.length - 1; i >= 0; i--) avgStatus += parseInt(markersInCircle[i].status);
+				avgStatus /= markersInCircle.length;
+				if (avgStatus < .3){
+					var color = 'red';
+				}	else if (avgStatus >= .3 && avgStatus <= .7){
+					var color = 'yellow';
+				}	else if (avgStatus > .7){
+					var color = 'green';
+				}
+				searchCircle.setOptions({ fillColor:color, strokeColor:color });
 			},
 			error: function(jqXHR){
 				console.log('error', jqXHR.responseText+' :: '+jqXHR.statusText);
@@ -60,14 +77,35 @@ GoogleMap = function()
 		});
 	}
 
-	var addMarker = function(obj, isMe)
+	var drawMarker = function(color)
+	{
+		return new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + color,
+			new google.maps.Size(21, 34),
+			new google.maps.Point(0,0),
+			new google.maps.Point(10, 34)
+		);
+	}
+	var drawMarkerShadow = function(){
+		return new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+		new google.maps.Size(40, 37),
+			new google.maps.Point(0, 0),
+			new google.maps.Point(12, 35)
+		);
+	}
+
+	var markerRed = drawMarker('B03120');
+	var markerGreen = drawMarker('24B020');
+	var markerShadow = drawMarkerShadow();
+
+	var addMarker = function(obj)
 	{
 		var m = new google.maps.Marker({
 			map : map,
 			isp : obj.isp,
 			status : obj.status,
 			time : obj.time,
-			special : isMe,
+			icon : obj.status == 1 ? markerGreen : markerRed,
+			shadow : markerShadow,
 			// animation : google.maps.Animation.DROP,
 			// icon : './img/markers/'+icons.fail[i]+'.png',
 			position : new google.maps.LatLng(obj.lat, obj.lng)
@@ -92,17 +130,17 @@ GoogleMap = function()
 
 	var drawCircle = function(lat, lng)
 	{
-		var circ = new google.maps.Circle({
+		searchCircle = new google.maps.Circle({
 			map: map,
 			strokeColor: "#FF0000",
 			strokeOpacity: 0.8,
 			strokeWeight: 2,
 			fillColor: "#FF0000",
-			fillOpacity: 0.35,
+			fillOpacity: 0.25,
 			radius: GoogleMap.calcMilesToMeters(searchArea / 2),
 			center: new google.maps.LatLng(lat, lng)
 		});
-		google.maps.event.addListener(circ, 'click', function(e) { win.close(); });
+		google.maps.event.addListener(searchCircle, 'click', function(e) { win.close(); });
 	}
 
 	var drawBounds = function(lat, lng)
@@ -118,6 +156,10 @@ GoogleMap = function()
 			fillOpacity: 0.35,
 			bounds: new google.maps.LatLngBounds(sw, ne)
 		});
+	}
+
+	google.maps.Circle.prototype.contains = function(latLng) {
+	  return this.getBounds().contains(latLng) && google.maps.geometry.spherical.computeDistanceBetween(this.getCenter(), latLng) <= this.getRadius();
 	}
 
 }
