46e97fc3f57b19d8c2fc97d6c988c31c77619e2a
Stephen Braitsch <stephen@quietless.com>
2012-07-10 12:07:08 -0700

    testing 3g bug

--- app/public/js/GoogleMap.js
+++ app/public/js/GoogleMap.js
@@ -117,7 +117,9 @@ GoogleMap = function()
 	{
 		console.log('adding markers')
 		for (var i = a.length - 1; i >= 0; i--) {
-		console.log(a[i].user.isp, a[i].status, a[i].user)
+		console.log(a[i].isp);
+		console.log(a[i].status)
+		console.log(a[i].user)
 	// build the markers and add them to the markers array //
 			if (a[i].user == false) {
 				addMarker(a[i]);
@@ -125,9 +127,9 @@ GoogleMap = function()
 				uMarker = drawGeoMarker(a[i]);
 			}
 		}
-		console.log('**markers = ',markers);
-		console.log('**markers.length = ',markers.length);
-		console.log('**marker = ',uMarker)
+		console.log('**markers = '+markers);
+		console.log('**markers.length = '+markers.length);
+		console.log('**marker = '+uMarker)
 		drawMap();
 	}
 	
