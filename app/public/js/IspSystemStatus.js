306423472cb7c616072d18f610221881372ee64b
Stephen Braitsch <stephen@quietless.com>
2012-07-10 12:23:33 -0700

    testing 3g bug

--- app/public/js/IspSystemStatus.js
+++ app/public/js/IspSystemStatus.js
@@ -30,9 +30,9 @@ $(document).ready(function(){
 		if (e){
 			console.log('onLocationDetected :: '+e);
 		} else{
-			return;
 			drawISPList();
 			map.setLocation( { isp : isp, status : status, lat : loc.lat, lng : loc.lng } );
+			return;
 			if (!initialized) {
 				map.getMarkers();
 				mdl.setLocation(loc.city, loc.state, isps);
