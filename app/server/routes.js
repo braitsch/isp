42679913b155428897bb360da3fd01a478ee20b1
braitsch <stephen@braitsch.io>
2018-08-26 16:39:55 -0700

    disabling 404 * to see if it is interfering with certbot

--- app/server/routes.js
+++ app/server/routes.js
@@ -100,8 +100,8 @@ module.exports = function(app) {
 		});
 	});
 	
-	app.get('*', function(req, res){
-		res.render('404', {  title: '404!' });
-	});
+	// app.get('*', function(req, res){
+	// 	res.render('404', {  title: '404!' });
+	// });
 
 };
